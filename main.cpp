6ed2b6bb4c4ac0efa2a792933dcf5bfd70a87e30
Yasen Angelov <yse33@abv.bg>
2025-01-19 18:22:01 +0200

    Add header comment block

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Yasen Angelov
+* @idnumber 7MI0600465
+* @compiler VC
+*
+* <movie program file>
+*
+*/
+
 #include <iostream>
 #include <fstream>
 
